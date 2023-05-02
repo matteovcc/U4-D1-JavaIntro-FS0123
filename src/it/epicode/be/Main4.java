23fbbb93cb7b3b3a11fd370e8ffd17325ee055a2
matteovcc <matteovcc99@gmail.com>
2023-05-02 16:18:28 +0200

    edit

--- src/it/epicode/be/Main4.java
+++ src/it/epicode/be/Main4.java
@@ -5,8 +5,13 @@ public class Main4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		pariDispari();
+        perimetro();
+
+	}
+	public static void pariDispari() {
 		Scanner in = new Scanner(System.in);
-        System.out.println("Inserisci un numero intero:");
+        System.out.println("Inserisci un numero:");
         int numero = in.nextInt();
         
         if (numero % 2 == 0) {
@@ -14,8 +19,16 @@ public class Main4 {
         }else {
         	System.out.println(1);
         }
-        in.close();
-
+        
+	}
+	
+	public static void perimetro() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Base");
+		double base = in.nextDouble();
+		System.out.println("Altezza");
+		double altezza = in.nextDouble();
+		System.out.println("il perimetro è:" + (base+altezza) *  2);
 	}
 
 
